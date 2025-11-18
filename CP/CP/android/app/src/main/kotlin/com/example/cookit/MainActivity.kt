a5a3947b9bd3e35efad95b73861240426554c230
Tuna <139547367+ChristinaTUNA@users.noreply.github.com>
2025-11-18 10:00:57 +0800

    Delete CP/CP directory

--- CP/CP/android/app/src/main/kotlin/com/example/cookit/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cookit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- CP/CP/android/app/src/main/kotlin/com/example/tocook_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cookit_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
